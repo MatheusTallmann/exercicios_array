91a6d69b7710e3573ed760ec608dff6117a29838
MatheusTallmann <matheusstma@gmail.com>
2020-10-08 21:15:41 -0300

    Exercis5

--- Arrays.cs
+++ Arrays.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,29 +29,102 @@ namespace exercicios_array
         {
             var ordens = new List<(double direto, double inverso)>();
 
-            var ordemDireta = ordens.Select(item => item.direto).ToList();
-
-            for (int i = 0; i < array.Count; i++)
+            var counter = 0;
+            for (int i = array.Count - 1; i >= 0 ; i--)
             {
-                ordemDireta.Add(array[i]);
-            }
-
-            array.Reverse();
-            for (int i = 0; i < array.Count; i++)
-            {
-                ordens.Add((ordemDireta[i], array[i]));
+                ordens.Add((array[counter], array[i]));
+                counter++;
             }
 
             return ordens;
+            // var ordemDireta = ordens.Select(item => item.direto).ToList();
+
+            // for (int i = 0; i < array.Count; i++)
+            // {
+            //     ordemDireta.Add(array[i]);
+            // }
+
+            // array.Reverse();
+            // for (int i = 0; i < array.Count; i++)
+            // {
+            //     ordens.Add((ordemDireta[i], array[i]));
+            // }
+
+            // return ordens;
         }
-        public string Exercises3(List<double> a, double number)
+        public string Exercise3(List<double> a, double number)
         // Leia 10 elementos e armazene em um array A. 
         // Em seguida, solicite pelo teclado um número qualquer e pesquise no array se o número existe. 
         // Caso, seja verdade imprima a mensagem: “O número existe no array”, caso contrário “Número inexistente”.
         {
-            return "";    
+            var search = a.Where(item => item == number).Count();
+            return search > 0 ? "O número existe no array" : "Número inexistente";
         }
-    
+
+        public bool Exercise4(List<double> a, List<double> b)
+        // Leia dois arrays A e B com 10 elementos.  
+        // Em seguida, compare os arrays e verifique se os mesmos são iguais ou diferentes.
+        {
+            var sum = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] == b[i])
+                {
+                    sum++;
+                }
+            }
+            if (sum == a.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public string Exercise5(List<double> a)
+        // Leia um array A com 15 elementos, e calcule a média aritmética dos mesmos, 
+        // em seguida, diga quantos dos elementos lidos estão abaixo, acima e na média.
+        {
+            var sum = 0.0;
+            var aboveAverage = 0;
+            var belowAverage = 0;
+            var onAverage = 0;
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                sum += a[i];
+            }
+            var average = sum / a.Count;
+            
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > average)
+                {
+                    aboveAverage++;
+                }
+                else if (a[i] < average)
+                {
+                    belowAverage++;
+                }
+                else if (a[i] == average)
+                {
+                    onAverage++;
+                }
+            }
+            return $"{aboveAverage}, {belowAverage}, {onAverage}";
+        }
+        public (int[], int[], int[]) Exercises6(List<double> a, List<double> b)
+        // Leia um array A com 12 elementos. Após sua leitura, 
+        // colocar os seus elementos em ordem crescente. 
+        // Depois ler um array B também com doze elementos, colocar os elementos de B em ordem decrescente. 
+        // Construir um array C, onde cada elemento de C é a soma do elemento correspondente de A com b. 
+        // Colocar em ordem crescente a matriz C e apresentar os seus valores.
+        {
+            a.Sort();
+
+        }
     }
 
 }
--- ArraysTest.cs
+++ ArraysTest.cs
@@ -20,7 +20,7 @@ namespace exercicios_array
             var expectedValue = exercises.Exercise1(a.ToList(), b.ToList());
 
             // Deve / Asserções
-            Assert.Equal(expectedValue, expectedC);
+            Assert.Equal(expectedC, expectedValue);
         }
 
         public static TheoryData<(List<double>, List<(double, double)>)> Exercise2Test()
@@ -53,6 +53,7 @@ namespace exercicios_array
         [Theory]
         [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, 4, "O número existe no array")]
         [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, 43, "Número inexistente")]
+        [InlineData(new double[10]{1,2,3,4,5.6,64,7.7,8,9,10}, 7.7, "O número existe no array")]
         public void Exercise3(double[] a, double number, string expected)
         // Leia 10 elementos e armazene em um array A. 
         // Em seguida, solicite pelo teclado um número qualquer e pesquise no array se o número existe. 
@@ -65,7 +66,60 @@ namespace exercicios_array
             var expectedValue = exercises.Exercise3(a.ToList(), number);
 
             // Deve / Asserções
-            Assert.Equal(expectedValue, expected);
+            Assert.Equal(expected, expectedValue);
+        }
+        [Theory]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, new double[10]{1,2,3,4,5,6,7,8,9,10}, true)]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10},new double[10]{1,2,2,4,5,44,7,4,9,11} , false)]
+        public void Exercise4(double[] a, double[] b, bool expected)
+        // Leia dois arrays A e B com 10 elementos.  
+        // Em seguida, compare os arrays e verifique se os mesmos são iguais ou diferentes.
+        {
+            // Dado / Setup
+            var exercises = new Arrays();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise4(a.ToList(), b.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+        [Theory]
+        [InlineData(new double[15]{0,0,0,0,0,8,8,8,8,8,8,8,8,8,8}, "10, 5, 0")]
+        [InlineData(new double[15]{10,5.5,0,0,0,7,7,8,8,8,8,8,8,8,8}, "11, 4, 0")]
+        [InlineData(new double[15]{10,9.5,6.5,4,4,3,9,7,8.5,5,8,8,8,8,8}, "9, 6, 0")]
+        public void Exercise5(double[] a, string expected)
+        // Leia um array A com 15 elementos, e calcule a média aritmética dos mesmos, 
+        // em seguida, diga quantos dos elementos lidos estão abaixo, acima e na média.
+        {
+            // Dado / Setup
+            var exercises = new Arrays();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise5(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+        [Theory]
+        [InlineData(new double[5]{1,2,3,4,5}, new double[5]{1,2,3,4,5},new double[5]{1,2,3,4,5},new double[5]{5,4,3,2,1}, new double[5]{0,0,0,0,0,})]
+        public void Exercise6(double[] a, double[] b, double[] expectedA, double[] expectedB, double[] expectedC)
+        // Leia um array A com 5 elementos. Após sua leitura, 
+        // colocar os seus elementos em ordem crescente. 
+        // Depois ler um array B também com 5 elementos, colocar os elementos de B em ordem decrescente. 
+        // Construir um array C, onde cada elemento de C é a soma do elemento correspondente de A com b. 
+        // Colocar em ordem crescente a matriz C e apresentar os seus valores.
+        {
+            // Dado / Setup
+            var exercises = new Arrays();
+
+            // Quando / Ação
+            (int[], int[], int[]) returnedValue = exercises.Exercise6(listA.ToList(), listB.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expectedA, returnedValue.Item1);
+            Assert.Equal(expectedB, returnedValue.Item2);
+            Assert.Equal(expectedC, returnedValue.Item3);
         }
     }
 }
