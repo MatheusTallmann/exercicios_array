72c9e94dee6601f9ebfac0fa047175b10ff8b36f
MatheusTallmann <matheusstma@gmail.com>
2020-11-12 21:13:31 -0300

    so gay

--- second_list/Arrays2.cs
+++ second_list/Arrays2.cs
@@ -39,6 +39,7 @@ namespace exercicios_array
             {
                 return "Há números repetidos";
             }
+            
             else
             {
                 return "Não há números repetidos";
@@ -46,33 +47,109 @@ namespace exercicios_array
         }
 
         public string Exercise3(List<string> nomes, List<string> masculinoOuFeminino, List<string> simOuNao)
+        // a aplicação deverá receber o sexo do entrevistado 
+        // e a sua resposta (sim ou não). Sabendo-se que foram entrevistadas 10 pessoas, fazer um algoritmo que calcule e escreva:
+        // O número de pessoas que responderam sim;
+        // O número de pessoas que responderam não;
+        // A porcentagem de pessoas do sexo feminino que responderam sim;
+        // A porcentagem de pessoas do sexo masculino que responderam não;
         {
-            var feminino = new List<string>();
-            var masculino = new List<string>();
-            var sim = new List<string>();
-            var nao = new List<string>();
-            
+            var sim = simOuNao.Where(item => item == "Sim").Count();
+            var nao = simOuNao.Where(item => item == "Não").Count();
+            var masculinoNao = 0;
+            var femininoSim = 0;
+
             for (int i = 0; i < nomes.Count; i++)
             {
-                if (masculinoOuFeminino[i] == "Masculino")
+                if (masculinoOuFeminino[i] == "Masculino" && simOuNao[i] == "Não")
                 {
-                    masculino.Add(masculinoOuFeminino[i]);
+                    masculinoNao++;
                 }
-                else
+                else if (masculinoOuFeminino[i] == "Feminino" && simOuNao[i] == "Sim")
                 {
-                    feminino.Add(masculinoOuFeminino[i]);
-                }
-
-                if (simOuNao[i] == "Sim")
-                {
-                    sim.Add(simOuNao[i]);
-                }
-                else
-                {
-                    nao.Add(simOuNao[i]);
+                    femininoSim++;
                 }
             }
+            double totalHomens = masculinoOuFeminino.Where(item => item == "Masculino").Count();
+            double totalMulheres = masculinoOuFeminino.Where(item => item == "Feminino").Count();
+
+            var porcentagemMasculinoNao = (masculinoNao * 100) / totalHomens;
+            var porcentagemFemininoSim = (femininoSim * 100) / totalMulheres;
+
+            return $"O número de pessoas que responderam sim: {sim} O número de pessoas que responderam não: {nao} A porcentagem de mulheres que responderam sim: {femininoSim}% A porcentagem de homens que responderam não: {masculinoNao}%";
+        }
+
+        public double Exercise4(List<double> a)
+        // A aplicação deverá receber uma matriz A do tipo vetor e Apresentar a soma de todos os elementos que sejam impares
+        {
+            return a.Where(item => item % 2 != 0).Sum();
+        }
+
+        public double Exercise5(List<double> a)
+        // A  aplicação deverá retornar quantos valores de um vetor de 10 posições são positivos
+        {
+            return a.Where(item => item > 0).Count();
+        }
+
+        public string Exercise6(List<double> a)
+        // A aplicação deverá retornar um vetor de 10 números positivos e escrever o valor do maior elemento e a respectiva posição que ele ocupa no vetor.
+        {
+            var maiorValor = a.Max();
+            var index = a.IndexOf(maiorValor);
+            return $"{maiorValor} e {index}";
+        } 
+
+        public int Exercise7(List<double> a) 
+        // A aplicação deverá retornar um vetor A e imprimir na tela quantas vezes há um número residindo na mesma posição do vetor que seu valor numérico.
+        {
+            var counter = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] == i)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+            // a.Where(item => item == a.IndexOf(item)).Count();
+        }
+
+        public int Exercise8(List<string> letras)
+        //Crie um vetor de strings de 10 posições onde cada posição recebe uma letra do alfabeto. 
+        //No final, imprima quantas destas são vogais.
+        {
+            var vogais = new List<string>{"a","e","i","o","u"};
+            var counter = 0;
+
+            foreach (var item in vogais)
+            {
+                for (int i = 0; i < letras.Count; i++)
+                {
+                    if (letras[i] == item)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
+        }
+
+        public string Exercise9(List<string> letras)
+        //Crie um vetor de strings de 10 posições onde cada posição recebe uma letra do alfabeto. 
+        //No final, imprima a string resultante da soma das strings que residem em índices pares.
+        {
+            var palavra = new List<string>();
+
+            for (int i = 0; i < letras.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    palavra.Add(letras[i]);        
+                }
+            }
+
+            return palavra.ToString();
         }
     }
-
 }
--- second_list/ArraysTest2.cs
+++ second_list/ArraysTest2.cs
@@ -39,8 +39,8 @@ namespace exercicios_array
         }
 
         [Theory]
-        [InlineData(new string[5]{"Matheus","Vinícius","Maria","Sara","Jana"}, new string[5]{"Masculino","Masculino","Feminino","Feminino","Feminino"},
-        new string[5]{"Sim","Sim","Não","Não","Não"})]
+        [InlineData(new string[5]{"Matheus","Vinícius","Maria","Pamela","Karina"}, new string[5]{"Masculino","Masculino","Feminino","Feminino","Feminino"},
+        new string[5]{"Sim","Sim","Não","Não","Não"}, "O número de pessoas que responderam sim: 2 O número de pessoas que responderam não: 3 A porcentagem de mulheres que responderam sim: 0% A porcentagem de homens que responderam não: 0%")]
         public void Exercise3(string[] nomes, string[] masculinoOuFeminino, string[] simOuNao, string expected)
         // a aplicação deverá receber o sexo do entrevistado 
         // e a sua resposta (sim ou não). Sabendo-se que foram entrevistadas 10 pessoas, fazer um algoritmo que calcule e escreva:
@@ -58,5 +58,114 @@ namespace exercicios_array
             // Deve / Asserções
             Assert.Equal(expected, expectedValue);
         }
+
+        [Theory]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, 25)]
+        [InlineData(new double[5]{2,2,2,4,5}, 5)]
+        [InlineData(new double[5]{2,2,1,4,5.5}, 6.5)]
+        public void Exercise4(double[] a, double expected)
+        // A aplicação deverá receber uma matriz A do tipo vetor e Apresentar a soma de todos os elementos que sejam impares
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise4(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+        
+        [Theory]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, 10)]
+        [InlineData(new double[10]{-1,-2,-3,-4,-5,6,7,8,9,10}, 5)]
+        public void Exercise5(double[] a, double expected)
+        // A  aplicação deverá retornar quantos valores de um vetor de 10 posições são positivos
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise5(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+
+        [Theory]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, "10 e 9")]
+        [InlineData(new double[10]{1,2,3,4,5345,6,7,8,9,10}, "5345 e 4")]
+        public void Exercise6(double[] a, string expected)
+        // A aplicação deverá retornar um vetor de 10 números positivos e escrever o valor do maior elemento e a respectiva posição que ele ocupa no vetor.
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise6(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+        
+        [Theory]
+        [InlineData(new double[10]{1,2,3,4,5,6,7,8,9,10}, 0)]
+        [InlineData(new double[10]{1,1,2,3,4,6,7,8,9,10}, 4)]
+        public void Exercise7(double[] a, int expected)
+        // A aplicação deverá retornar um vetor A e imprimir na tela quantas vezes há um número residindo na mesma posição do vetor que seu valor numérico.
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var expectedValue = exercises.Exercise7(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, expectedValue);
+        }
+        
+        [Theory]
+        [InlineData(new string[6]{"a","b","c","d","e","f"}, 2)]
+        public void Exercise8(string[] letras, int expected)
+        //Crie um vetor de strings de 10 posições onde cada posição recebe uma letra do alfabeto. 
+        //No final, imprima quantas destas são vogais.
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var atualValue = exercises.Exercise8(a.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, atualValue);
+        }        
+
+        [Theory]
+        [InlineData(new string[25]{"M","a","A","a","T","a","H","a","E","a","U","a","S","a"," ","a","É","a"," ","a","G","a","A","a","Y"}, "MATHEUS É GAY")]
+        [InlineData(new string[27]{"V","a","I","a","N","a","I","a","C","a","I","a","U","a","S","a"," ","a","T","a","B","a","M","a"," ","a","É",}, "VINICIUS TBM É")]
+        public void Exercise9(string[] letras, string expected)
+        //Crie um vetor de strings de 10 posições onde cada posição recebe uma letra do alfabeto. 
+        //No final, imprima a string resultante da soma das strings que residem em índices pares.
+        {
+            // Dado / Setup
+            var exercises = new Arrays2();
+
+            // Quando / Ação
+            var atualValue = exercises.Exercise9(letras.ToList());
+
+            // Deve / Asserções
+            Assert.Equal(expected, atualValue);
+        }
+
+        // public void Exercise9(List<double> a) 
+        // // A aplicação deverá retornar um vetor A e imprimir na tela quantas vezes há um número residindo na mesma posição do vetor que seu valor numérico.
+        // {
+        //     a.Select(item =>
+        //     {
+        //         return item == a.IndexOf(item);
+        //     }).Count();
+            
+        //     a.Where(item => item == a.IndexOf(item)).Count();
+        // }
     }
 }
